814f55774b6b5e0075ecf72d03b8b0bdebe7e47e
Kumru Çelik <celikumru13@gmail.com>
2026-02-24 19:52:23 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: [Kumru Çelik]</p>
+<p>Ogrenci No: [220541126]</p>
+</div>
+) 
+}
+export default App
