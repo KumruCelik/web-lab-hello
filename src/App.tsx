079642564e8d363345b213e211768ba4dc668f0c
Kumru Çelik <celikumru13@gmail.com>
2026-02-24 21:08:00 +0300

    feat: update homepage title and faculty info

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
 return (
 <div>
 <h1>Web Tasarimi ve Programlama</h1>
+<h1>Teknoloji Fakültesi Yazılım Mühendisliği</h1>
 <h2>LAB-1</h2>
 <p>Ad Soyad: [Kumru Çelik]</p>
 <p>Ogrenci No: [220541126]</p>
